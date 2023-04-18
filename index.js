9e95ea05c6b5d532b5a0d8c6fa1badf70d8723d3
Brandon <rbrandon087@gmail.com>
2023-04-17 22:04:43 -0700

    I have updated my code and got my server to run

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+app.use ('/places', require ('./Controller/places'))
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
