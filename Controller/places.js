0177836a701881fa1862eb307622e3cd461c6ad4
Brandon <rbrandon087@gmail.com>
2023-04-20 20:28:08 -0700

    Jazzed up form page

--- Controller/places.js
+++ Controller/places.js
@@ -23,6 +23,9 @@ router.get('/', (req,res) => {
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
-
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('POST /places')
+})
 
 module.exports = router
