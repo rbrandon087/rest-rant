bd03fe14b204f18adeb9e689b52a3966d774b356
Brandon <rbrandon087@gmail.com>
2023-04-23 10:23:20 -0700

    adjusted my code in places.js my edit and delete were not showing

--- Controller/places.js
+++ Controller/places.js
@@ -1,46 +1,26 @@
-const router = require('express').Router()
+const router = require ('express').Router()
 const places = require('../models/places.js')
 
-
-router.get('/', (req,res) => {
-  let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/thai.jpg'
-  }, {
-      name: 'Coding Cat Cafe',
-      city: 'Phoenix',
-      state: 'AZ',
-      cuisines: 'Coffee, Bakery',
-      pic: '/images/cafe.jpg'
-  }]
-  
-    res.render('places/index', {places})
+router.get('/', (req,res)=>{
+    // let places = [{
+    //     name: 'H-Thai-ML',
+    //     city: 'Seattle',
+    //     state: 'WA',
+    //     cuisines: 'Thai, Pan-Asian',
+    //     pic: '/images/pizza.jpg'
+    //   }, {
+    //     name: 'Coding Cat Cafe',
+    //     city: 'Phoenix',
+    //     state: 'AZ',
+    //     cuisines: 'Coffee, Bakery',
+    //     pic: '/images/veggies.jpg'
+    //   }]
+      
+    res.render('places/index',{places})
 })
-
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
-
-
-router.post('/', (req, res) => {
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = 'https://images.unsplash.com/photo-1526336024174-e58f5cdd8e13?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
-  }
-  if (!req.body.city) {
-    req.body.city = ''
-  }
-  if (!req.body.state) {
-    req.body.state = 'USA'
-  }
-  places.push(req.body)
-  res.redirect('/places')
-})
-
-
 router.delete('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
@@ -54,6 +34,77 @@ router.delete('/:id', (req, res) => {
     res.redirect('/places')
   }
 })
+
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show' , {place: places [id], id} )
+  }
+})
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id], id })
+  }
+})
+
+
+
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+      // Dig into req.body and make sure data is valid
+      if (!req.body.pic) {
+          // Default image if one is not provided
+          req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+
+      // Save the new data into places[id]
+      places[id] = req.body
+      res.redirect(`/places/${id}`)
+  }
+})
+
 
 
 
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,16 +6,19 @@ function show(data) {
     <Def>
       <main>
         <h1>{data.place.name}</h1>
+        <h2>Rating</h2>
+        <p>Not Rated</p>
+        <h2>Description</h2>
+        <p>No</p>
+        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+          Edit
+        </a>
+        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+          <button type="submit" className="btn btn-danger">
+            Delete
+          </button>
+        </form>
       </main>
-      <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-        Edit
-      </a>
-
-      <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-        <button type="submit" className="btn btn-danger">
-          Delete
-        </button>
-      </form>
     </Def>
   );
 }
