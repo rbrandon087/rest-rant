b4d163c002db945aa1068faf3071a9531d8c0b9d
Brandon <rbrandon087@gmail.com>
2023-05-03 21:56:17 -0700

    adjusted my code

--- Controller/places.js
+++ Controller/places.js
@@ -12,29 +12,16 @@ router.get('/', (req, res) => {
     })
 })
 
-
 router.post('/', (req, res) => {
   db.Place.create(req.body)
   .then(() => {
       res.redirect('/places')
   })
   .catch(err => {
-    if (err && err.name == 'ValidationError') {
-      let message = 'Validation Error: '
-      for (var field in err.errors) {
-          message += `${field} was ${err.errors[field].value}. `
-          message += `${err.errors[field].message}`
-      }
-      console.log('Validation error message', message)
-      res.render('places/new', { message })
-  }
-  else {
+      console.log('err', err)
       res.render('error404')
-  }
   })
 })
-
-
 
 
 router.get('/new', (req, res) => {
@@ -43,9 +30,7 @@ router.get('/new', (req, res) => {
 
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
-  .populate('comments')
   .then(place => {
-      console.log(place.comments)
       res.render('places/show', { place })
   })
   .catch(err => {
@@ -53,29 +38,6 @@ router.get('/:id', (req, res) => {
       res.render('error404')
   })
 })
-
-router.post('/:id/comment', (req, res) => {
-  console.log(req.body)
-  db.Place.findById(req.params.id)
-  .then(place => {
-      db.Comment.create(req.body)
-      .then(comment => {
-          place.comments.push(comment.id)
-          place.save()
-          .then(() => {
-              res.redirect(`/places/${req.params.id}`)
-          })
-      })
-      .catch(err => {
-          res.render('error404')
-      })
-  })
-  .catch(err => {
-      res.render('error404')
-  })
-})
-
-
 
 
 router.put('/:id', (req, res) => {
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -2,92 +2,50 @@ const React = require('react')
 const Def = require('../default')
 
 function new_form (data) {
+    let message = ''
+    if (data.message) {
+      message = (
+        <h4 className="alert-danger">
+          {data.message}
+        </h4>
+      )
+    }
     return (
         <Def>
           <main>
             <h1>Add a New Place</h1>
+            {message}
             <form method="POST" action="/places">
-            <div className="form-group">
-                <label for="founded">Founded Year</label>
-                <input type="number" className="form-control" id="founded" name="founded" value={new Date() .getFullYear()} />
-            </div>
-
-            <div className="form-group">
-                <label htmlFor="name">Place Name</label>
-                <input className="form-control" id="name" name="name" required />
-            </div>
-            <div className="form-group">
-                <label htmlFor="pic">Place Picture</label>
-                <input className="form-control" id="pic" name="pic" />
-            </div>
-            <div className="form-group">
-                <label htmlFor="city">City</label>
-                <input className="form-control" id="city" name="city" />
-            </div>
-            <div className='form-group'>
-                        <label htmlFor="state">State</label>
-                        <select name="state" id="state" className="form-control">
-                            <option value="filler" selected>Select a State</option>
-                            <option value="Alabama">AL</option>
-                            <option value="Alaska">AK</option>
-                            <option value="Arizona">AZ</option>
-                            <option value="Arkansas">AR</option>
-                            <option value="California">CA</option>
-                            <option value="Colorado">CO</option>
-                            <option value="Conneticut">CT</option>
-                            <option value="Delaware">DE</option>
-                            <option value="Florida">FL</option>
-                            <option value="Georgia">GA</option>
-                            <option value="Hawaii">HI</option>
-                            <option value="Idaho">ID</option>
-                            <option value="Illinois">IL</option>
-                            <option value="Indiana">IN</option>
-                            <option value="Iowa">IA</option>
-                            <option value="Kansas">KS</option>
-                            <option value="Kentucky">KY</option>
-                            <option value="Louisiana">LA</option>
-                            <option value="Maine">ME</option>
-                            <option value="Maryland">MD</option>
-                            <option value="Massachusetts">MA</option>
-                            <option value="Michigan">MI</option>
-                            <option value="Minnesota">MN</option>
-                            <option value="Mississippi">MS</option>
-                            <option value="Missouri">MO</option>
-                            <option value="Montana">MT</option>
-                            <option value="Nebraska">NE</option>
-                            <option value="Nevada">NV</option>
-                            <option value="New Hampshire">NH</option>
-                            <option value="New Jersey">NJ</option>
-                            <option value="New Mexico">NM</option>
-                            <option value="New York">NY</option>
-                            <option value="North Carolina">NC</option>
-                            <option value="North Dakota">ND</option>
-                            <option value="Ohio">OH</option>
-                            <option value="Oklahoma">OK</option>
-                            <option value="Oregon">OR</option>
-                            <option value="Pennsylvania">PA</option>
-                            <option value="Rhode Island">RI</option>
-                            <option value="South Carolina">SC</option>
-                            <option value="South Dakota">SD</option>
-                            <option value="Tennessee">TN</option>
-                            <option value="Texas">TX</option>
-                            <option value="Utah">UT</option>
-                            <option value="Vermont">VT</option>
-                            <option value="Virginia">VA</option>
-                            <option value="Washington">WA</option>
-                            <option value="West Virginia">WV</option>
-                            <option value="Wisconsin">WI</option>
-                            <option value="Wyoming">WY</option>
-                        </select>
-                    </div>
-            <div className="form-group">
+              <div className="row">
+                <div className="form-group col-sm-6">
+                  <label htmlFor="name">Place Name</label>
+                  <input className="form-control" id="name" name="name" required />
+                </div>
+                <div className="form-group col-sm-6">
+                  <label htmlFor="pic">Place Picture</label>
+                  <input className="form-control" id="pic" name="pic" />
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-group col-sm-4">
+                  <label htmlFor="city">City</label>
+                  <input className="form-control" id="city" name="city" />
+                </div>
+                <div className="form-group col-sm-4">
+                  <label htmlFor="state">State</label>
+                  <input className="form-control" id="state" name="state" />
+                </div>
+                <div className="form-group col-sm-4">
+                  <label htmlFor="founded">Founded Year</label>
+                  <input type="number" className="form-control" id="founded" name="founded" defaultValue={new Date().getFullYear()} />
+                </div>
+              </div>
+              <div className="form-group">
                 <label htmlFor="cuisines">Cuisines</label>
                 <input className="form-control" id="cuisines" name="cuisines" required />
-            </div>
-            <input className="btn btn-primary" type="submit" value="Add Place" />
+              </div>
+              <input className="btn btn-primary" type="submit" value="Add Place" />
             </form>
-
-
           </main>
         </Def>
     )
